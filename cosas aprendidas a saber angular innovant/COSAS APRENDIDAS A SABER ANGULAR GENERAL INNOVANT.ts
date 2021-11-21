90fadb28ba04a1280f32655f2a999d3041737951
Rafael <zerazzadar@gmail.com>
2021-11-20 23:16:01 -0500

    Poniendo cdn Bootstrap

--- cosas aprendidas a saber angular innovant/COSAS APRENDIDAS A SABER ANGULAR GENERAL INNOVANT.ts
+++ cosas aprendidas a saber angular innovant/COSAS APRENDIDAS A SABER ANGULAR GENERAL INNOVANT.ts
@@ -111,6 +111,13 @@ export class DetailsProductComponent implements OnInit {
 [HTML]
 <span >el id es {{numberid}} </span>
 
+--------------------------------------------
+:INCLUIR CDN BOOTSTRAP:
+--------------------------------------------
+Buscas el archivo Index.html
+debajo del </body> pones esto
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 ===================================================
 // *********************************************************************
 // ************************** ANGULAR 8 ********************************
